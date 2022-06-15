98e15d48d9d8a8ea647a7ec5ce7a684059a8c377
Heezol06 <shahriarrahmanheezol@gmail.com>
2022-06-15 15:38:49 +0600

    added-navbar

---
+++ src/component/Home/AboutMomento/AboutMomento.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutMomento = () => {
+    return (
+        <div class=" container mx-auto grid grid-rows-2 md:grid-rows-6 md:px-5 px-20">
+            <h4 className='text-xl font-bold'>We are Monteno NFT</h4>
+        </div>
+    );
+};
+
+export default AboutMomento;
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Common/Navbar';
+import AboutMomento from './AboutMomento/AboutMomento';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <AboutMomento></AboutMomento>
         </div>
     );
 };
